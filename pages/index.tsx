2aaccdf580ca0c374ba88addba6daae4b0878a9b
Liu Dake <lolitacon2012@gmail.com>
2021-08-18 18:43:27 +0800

    chore: update read me

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,44 +32,6 @@ export default function Home() {
           <Button onClick={() => {
             router.push('/home')
           }} >{t('homepage_enter')}</Button>
-
-          {/* <div className={styles.grid}>
-            <a className={styles.card}>
-              <h2>{t('homepage_subtitle_create')} ✍</h2>
-              <p>{t('homepage_subtitle_create_intro')}</p>
-            </a>
-
-
-            <a onClick={(e) => {
-              e.preventDefault();
-              router.push('/home')
-            }} className={styles.card}>
-              <h2>{t('homepage_subtitle_learn')} 🏆</h2>
-              <p>{t('homepage_subtitle_learn_intro')}</p>
-            </a>
-
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                router.push('/home')
-              }}
-              className={styles.card}
-            >
-              <h2>{t('homepage_subtitle_share')} 🐾</h2>
-              <p>{t('homepage_subtitle_share_intro')}</p>
-            </a>
-
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                window.location.href = 'https://www.buymeacoffee.com/liudake'
-              }}
-              className={styles.card}
-            >
-              <h2>{t('homepage_subtitle_support')} ☕</h2>
-              <p>{t('homepage_subtitle_support_intro')}</p>
-            </a>
-          </div> */}
         </div>
       </Container>
     </div>
