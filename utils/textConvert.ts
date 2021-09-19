77507394f9bb2ab0ab71c8f097219360a0395e06
Liu Dake <lolitacon2012@gmail.com>
2021-09-19 16:05:52 +0800

    fix: ~

--- utils/textConvert.ts
+++ utils/textConvert.ts
@@ -1,7 +1,7 @@
 const toHalfWidth = (input: string) => {
-    if (input === '～' || input === '∼' || input === '~') {
+    if (input === '～' || input === '∼' || input === '~' || input === '〜') {
         return '~';
-    } else if(input === '―' || input === '‐') {
+    } else if (input === '-' || input === '―' || input === '‐' || input === "ー" || input === "－" || input === "—" || input === "⁻") {
         return '-';
     } else {
         return input;
